3580d92fac7890c01cf83ebb571a167f514bfe34
Chris1125 <wangyang9125@163.com>
2020-11-11 23:45:50 +0800

    2020-11-11 每日一题：LeetCode 514

---
+++ src/main/java/Q0500_0599/Q0514/Solution.java
@@ -0,0 +1,24 @@
+package Q0500_0599.Q0514;
+
+/**
+ * @author Chris
+ * @description <a href="https://leetcode-cn.com/problems/freedom-trail/">自由之路</a>
+ * Difficulty: Hard
+ * Status: TODO
+ * Daily: 2020-11-11
+ * @since 2020-11-11
+ */
+public class Solution {
+
+    public static void main(String[] args) {
+        String ring = "godding";
+        String key = "gd";
+
+        System.out.println(findRotateSteps(ring, key));
+    }
+
+    public  static  int findRotateSteps(String ring, String key) {
+        return 0;
+    }
+
+}

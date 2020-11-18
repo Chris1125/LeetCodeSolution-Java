8a5dc3c7cb24cc000f32d7b0c62a71922f0b0c0f
Chris1125 <wangyang9125@163.com>
2020-11-19 00:01:31 +0800

    2020-11-16 LeetCode 406 每日一题

---
+++ src/main/java/Q0400_0499/Q0406/Solution.java
@@ -0,0 +1,21 @@
+package Q0400_0499.Q0406;
+
+/**
+ * @author Chris
+ * @description <a href="https://leetcode-cn.com/problems/queue-reconstruction-by-height/">根据身高重建队列</a>
+ * Difficulty: Medium
+ * Status: TODO
+ * Daily: 2020-11-16
+ * @since 2020-11-16
+ */
+public class Solution {
+
+    public static void main(String[] args) {
+        // [[7,0], [4,4], [7,1], [5,0], [6,1], [5,2]]
+    }
+
+    public int[][] reconstructQueue(int[][] people) {
+        return null;
+    }
+
+}

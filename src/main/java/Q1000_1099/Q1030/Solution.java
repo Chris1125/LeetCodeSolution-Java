eec67e55c9f002623306522abe0c6f79cd32c5c1
Chris1125 <wangyang9125@163.com>
2020-11-19 00:01:53 +0800

    2020-11-17 LeetCode 1030 每日一题

---
+++ src/main/java/Q1000_1099/Q1030/Solution.java
@@ -0,0 +1,25 @@
+package Q1000_1099.Q1030;
+
+import java.util.Arrays;
+
+/**
+ * @author Chris
+ * @description <a href="https://leetcode-cn.com/problems/matrix-cells-in-distance-order/">距离顺序排列矩阵单元格</a>
+ * Difficulty: Medium
+ * Status: TODO
+ * Daily: 2020-11-17
+ * @since 2020-11-17
+ */
+public class Solution {
+
+    public static void main(String[] args) {
+        int R = 1, C = 2, r0 = 0, c0 = 0;
+
+        System.out.println(Arrays.deepToString(allCellsDistOrder(R, C, r0, c0)));
+    }
+
+    public static int[][] allCellsDistOrder(int R, int C, int r0, int c0) {
+        return null;
+    }
+
+}

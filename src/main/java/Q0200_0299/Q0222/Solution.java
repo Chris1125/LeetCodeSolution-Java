627e45c03843421e7667270f9d79aa6e071cb4f9
Chris1125 <wangyang9125@163.com>
2020-11-25 23:41:07 +0800

    2020-11-24 LeetCode 222 每日一题

---
+++ src/main/java/Q0200_0299/Q0222/Solution.java
@@ -0,0 +1,19 @@
+package Q0200_0299.Q0222;
+
+import base.TreeNode;
+
+/**
+ * @author Chris
+ * @description <a href="https://leetcode-cn.com/problems/count-complete-tree-nodes/">完全二叉树的节点个数</a>
+ * Difficulty: Medium
+ * Status: TODO
+ * Daily: 2020-11-24
+ * @since 2020-11-24
+ */
+public class Solution {
+
+    public int countNodes(TreeNode root) {
+        return 0;
+    }
+
+}

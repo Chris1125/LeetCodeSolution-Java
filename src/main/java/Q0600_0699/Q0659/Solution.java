4b74309f0e1833cb9c6a0fe898a3847a64a6284f
Chris1125 <wangyang9125@163.com>
2020-12-05 22:29:18 +0800

    2020-12-04 LeetCode 659 每日一题

---
+++ src/main/java/Q0600_0699/Q0659/Solution.java
@@ -0,0 +1,17 @@
+package Q0600_0699.Q0659;
+
+/**
+ * @author Chris
+ * @description <a href="https://leetcode-cn.com/problems/split-array-into-consecutive-subsequences/">分割数组为连续子数列</a>
+ * Difficulty: Medium
+ * Status: TODO
+ * Daily: 2020-12-04
+ * @since 2020-12-04
+ */
+public class Solution {
+
+    public boolean isPossible(int[] nums) {
+        return false;
+    }
+
+}

8b1bb190ed97e6104c0709ddfbe49264e586672c
Chris1125 <wangyang9125@163.com>
2020-12-09 16:36:24 +0800

    2020-12-07 LeetCode 861 每日一题

---
+++ src/main/java/Q0800_0899/Q0861/Solution.java
@@ -0,0 +1,17 @@
+package Q0800_0899.Q0861;
+
+/**
+ * @author Chris
+ * @description <a href="https://leetcode-cn.com/problems/score-after-flipping-matrix/">翻转矩阵后的得分</a>
+ * Difficulty: Medium
+ * Status: TODO
+ * Daily: 2020-12-07
+ * @since 2020-12-07
+ */
+public class Solution {
+
+    public int matrixScore(int[][] A) {
+        return 0;
+    }
+
+}

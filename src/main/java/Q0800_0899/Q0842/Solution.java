dabe79dc0f7763ba2da0d7fac09a0656e84e95fb
Chris1125 <wangyang9125@163.com>
2020-12-15 00:37:58 +0800

    2020-12-08 LeetCode 842 每日一题

---
+++ src/main/java/Q0800_0899/Q0842/Solution.java
@@ -0,0 +1,19 @@
+package Q0800_0899.Q0842;
+
+import java.util.List;
+
+/**
+ * @author Chris
+ * @description <a href="https://leetcode-cn.com/problems/split-array-into-fibonacci-sequence/">将数组拆分成斐波那契序列</a>
+ * Difficulty: Medium
+ * Status: TODO
+ * Daily: 2020-12-08
+ * @since 2020-12-08
+ */
+public class Solution {
+
+    public List<Integer> splitIntoFibonacci(String S) {
+        return null;
+    }
+
+}

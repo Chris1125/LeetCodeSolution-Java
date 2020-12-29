c4ae21b4146e62eaf86b3ddb71b06610c79e2fdd
Chris1125 <wangyang9125@163.com>
2020-12-29 18:29:51 +0800

    2020-12-28 LeetCode 188 每日一题

---
+++ src/main/java/Q0100_0199/Q0188/Solution.java
@@ -0,0 +1,24 @@
+package Q0100_0199.Q0188;
+
+/**
+ * @author Chris
+ * @description <a href="https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock-iv/">买卖股票的最佳时机 IV</a>
+ * Difficulty: Hard
+ * Status:
+ * Daily: 2020-12-28
+ * @since 2020-12-28
+ */
+public class Solution {
+
+    public static void main(String[] args) {
+        int k = 2;
+        int[] prices = new int[]{2, 4, 1};
+
+        System.out.println(maxProfit(k, prices));
+    }
+
+    public static int maxProfit(int k, int[] prices) {
+        return 0;
+    }
+
+}

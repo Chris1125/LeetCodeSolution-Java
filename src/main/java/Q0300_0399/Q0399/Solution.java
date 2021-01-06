b903c3abecdd69c24ac6011a6c84fc7af3b8850d
wangyang <wangyang9125@163.com>
2021-01-06 18:20:23 +0800

    2021-01-06 LeetCode 399 每日一题

---
+++ src/main/java/Q0300_0399/Q0399/Solution.java
@@ -0,0 +1,22 @@
+package Q0300_0399.Q0399;
+
+import java.util.List;
+
+/**
+ * @author Chris
+ * @description <a href="https://leetcode-cn.com/problems/evaluate-division/">除法求值</a>
+ * Difficulty: Medium
+ * Status: TODO
+ * Daily: 2021-01-06
+ * @since 2021-01-06
+ */
+public class Solution {
+
+    public static void main(String[] args) {
+
+    }
+
+    public double[] calcEquation(List<List<String>> equations, double[] values, List<List<String>> queries) {
+        return null;
+    }
+}

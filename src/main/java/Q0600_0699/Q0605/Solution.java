20cbdb2dd1c15e46b8a7b90839c3f467ec5fbf4c
wangyang <wangyang9125@163.com>
2021-01-06 18:18:50 +0800

    2021-01-01 LeetCode 605 每日一题

---
+++ src/main/java/Q0600_0699/Q0605/Solution.java
@@ -0,0 +1,21 @@
+package Q0600_0699.Q0605;
+
+/**
+ * @author Chris
+ * @description <a href="https://leetcode-cn.com/problems/can-place-flowers/">种花问题</a>
+ * Difficulty: Easy
+ * Status: TODO
+ * Daily: 2021-01-01
+ * @since 2021-01-01
+ */
+public class Solution {
+
+    public static void main(String[] args) {
+
+    }
+
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        return false;
+    }
+
+}
